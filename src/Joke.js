e9d158c1ce4ce8ec543753c34c46292176986fa1
Kenny Hwu <kennywhwu@gmail.com>
2018-11-05 14:54:19 -0800

    added thumbs up and down icons

--- src/Joke.js
+++ src/Joke.js
@@ -2,16 +2,23 @@ import React, { Component } from 'react';
 import './Joke.css';
 
 class Joke extends Component {
+  // static defaultProps = {
+  //   votes: 0
+  // };
+
   render() {
     return (
       <div className="Joke">
-        {this.props.joke}
-        <button className="up" onClick={() => this.props.handleClick('up')}>
-          Up
-        </button>
-        <button className="down" onClick={() => this.props.handleClick('down')}>
-          Down
-        </button>
+        {this.props.joke}{' '}
+        <i
+          className="up fas fa-thumbs-up"
+          onClick={() => this.props.handleClick('up', this.props.id)}
+        />{' '}
+        <i
+          className="down fas fa-thumbs-down"
+          onClick={() => this.props.handleClick('down', this.props.id)}
+        />{' '}
+        {this.props.votes}
       </div>
     );
   }
--- src/JokeList.js
+++ src/JokeList.js
@@ -2,45 +2,117 @@ import React, { Component } from 'react';
 import './JokeList.css';
 import Joke from './Joke';
 import axios from 'axios';
-import uuid from 'uuid/v4';
+// import uuid from 'uuid/v4';
 
 class JokeList extends Component {
   constructor(props) {
     super(props);
-    this.state = { votes: [], isLoading: true };
+    this.state = { jokes: [], isLoading: true };
     this.vote = this.vote.bind(this);
+    this.refreshList = this.refreshList.bind(this);
   }
 
   async componentDidMount() {
-    this.jokes = [];
-    for (let i = 0; i < 10; i++) {
-      await this.requestJoke();
+    if (JSON.parse(localStorage.getItem('jokes'))) {
+      this.setState({
+        jokes: JSON.parse(localStorage.getItem('jokes')),
+        isLoading: false
+      });
+    } else {
+      let jokes = [];
+      for (let i = 0; i < 10; i++) {
+        await this.requestJoke(jokes);
+      }
+      this.setState({ jokes, isLoading: false });
     }
-    this.setState({ isLoading: false });
   }
 
-  async requestJoke() {
+  componentDidUpdate(prevProps, prevState) {
+    localStorage.setItem('jokes', JSON.stringify(this.state.jokes));
+  }
+
+  async requestJoke(jokes) {
     let response = await axios.get('https://icanhazdadjoke.com/', {
       headers: { Accept: 'application/json' }
     });
-    let joke = { joke: response.data.joke, id: uuid() };
-    this.jokes.push(joke);
-    console.log(this.jokes);
+    let joke = { joke: response.data.joke, id: response.data.id, votes: 0 };
+
+    if (jokes.every((e, i) => e.id !== joke.id)) {
+      jokes.push(joke);
+    } else {
+      await this.requestJoke(jokes);
+    }
   }
 
-  vote(direction, id) {}
+  vote(direction, id) {
+    console.log(direction, id);
+    let newJoke = {
+      joke: this.state.jokes[id].joke,
+      votes: this.state.jokes[id].votes,
+      id: this.state.jokes[id].id
+    };
 
-  render() {
-    let jokes;
-    if (this.jokes) {
-      jokes = this.jokes.map((e, i) => (
-        <Joke joke={e.joke} key={e.id} handleClick={this.vote} />
-      ));
+    // Change vote total based on direction
+    if (direction === 'up') {
+      newJoke.votes += 1;
+    } else {
+      newJoke.votes -= 1;
     }
 
-    return (
-      <div className="JokeList">{this.state.isLoading ? undefined : jokes}</div>
-    );
+    // Pull in jokes array from current state
+    let votedJokes = {
+      jokes: [
+        ...this.state.jokes.slice(0, id),
+        newJoke,
+        ...this.state.jokes.slice(id + 1)
+      ]
+    };
+
+    // Order jokes based on reverse order vote
+    let orderedJokes = votedJokes.jokes.sort((a, b) => b.votes - a.votes);
+
+    // Set state
+    this.setState(st => ({
+      jokes: orderedJokes
+    }));
+  }
+
+  async refreshList() {
+    localStorage.removeItem('jokes');
+    this.setState({ isLoading: true });
+    let jokes = [];
+    for (let i = 0; i < 10; i++) {
+      await this.requestJoke(jokes);
+    }
+    this.setState({ jokes, isLoading: false });
+  }
+
+  render() {
+    let result;
+    let jokes;
+    if (!this.state.isLoading) {
+      jokes = this.state.jokes.map((e, i) => (
+        <Joke
+          joke={e.joke}
+          key={e.id}
+          id={i}
+          handleClick={this.vote}
+          votes={e.votes}
+        />
+      ));
+      result = (
+        <div>
+          {jokes}
+          <button onClick={this.refreshList}>Refresh Jokes</button>
+        </div>
+      );
+    } else {
+      result = (
+        <h1>It's loading already, can you just wait a minute please???</h1>
+      );
+    }
+
+    return <div className="JokeList">{result}</div>;
   }
 }
 
